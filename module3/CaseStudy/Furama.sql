cd875d9c4906ccd2efc7d59b4748aaff0598d645
unknown <hanhly288@gmail.com>
2021-05-28 21:14:05 +0700

    new

--- module3/CaseStudy/Furama.sql
+++ module3/CaseStudy/Furama.sql
@@ -203,14 +203,158 @@ where  (EmployeeName like 'H%' or  EmployeeName like  'T%' or  EmployeeName like
 
 select * 
 from Customer
-where  (year(now()) - year(CustomerBir) >=18 AND year(now()) - year(CustomerBir) <=40) AND ( Address = 'Da Nang' OR Address = 'Quang Tri'); 
+where  18<=timestampdiff(YEAR, CustomerBir, now())<=50 AND ( Address = 'Da Nang' OR Address = 'Quang Tri'); 
 
 -- 4 Đếm xem tương ứng với mỗi khách hàng đã từng đặt phòng bao nhiêu lần. Kết quả hiển thị được sắp xếp tăng dần theo số lần đặt phòng của khách hàng. Chỉ đếm những khách hàng nào có Tên loại khách hàng là “Diamond”
 
-select  CustomerName, Contract.ContractId , count(*) as ContractQuantity from (Contract 
-inner join Customer on Customer.CustomerId = Contract.CustomerId)
-inner join CustomerType on  CustomerType.CustomerTypeId = Customer.CustomerTypeId 
-where CustomerTypeName = 'Diamond' 
-group by CustomerName 
+select  Customer.CustomerName, count(Customer.CustomerName) as ContractQuantity from Contract 
+inner join Customer on Customer.CustomerId = Contract.CustomerId
+where CustomerTypeID = 'Di' 
+group by Customer.CustomerName 
 order by ContractQuantity;
 
+-- 5 Hiển thị IDKhachHang, HoTen, TenLoaiKhach, IDHopDong, TenDichVu, NgayLamHopDong, NgayKetThuc, TongTien (Với TongTien được tính theo công thức như sau: ChiPhiThue + SoLuong*Gia, với SoLuong và Giá là từ bảng DichVuDiKem) cho tất cả các Khách hàng đã từng đặt phỏng. (Những Khách hàng nào chưa từng đặt phòng cũng phải hiển thị ra).
+
+select Customer.CustomerId, Customer.CustomerName, CustomerType.CustomerTypeName, Contract.ContractID, Service.ServiceName, Contract.BeginDay,  Contract.FinishDay, Service.RentalPrice +(DetailContract.Quantity*AccompaniedService.Price) as TotalPayment
+from Customer
+left join Contract
+on Customer.CustomerId=Contract.CustomerId
+left join Service on Contract.ServiceId=Service.ServiceId
+left join DetailContract on Contract.ContractId=DetailContract.ContractId
+left join AccompaniedService on DetailContract.AccSerId=AccompaniedService.AccSerId
+left join CustomerType on Customer.CustomerTypeId=CustomerType.CustomerTypeId;
+
+-- 6 Hiển thị IDDichVu, TenDichVu, DienTich, ChiPhiThue, TenLoaiDichVu của tất cả các loại Dịch vụ chưa từng được Khách hàng thực hiện đặt từ quý 1 của năm 2019 (Quý 1 là tháng 1, 2, 3).
+
+select Service.ServiceId, Service.ServiceName, Service.UseArea, Service.RentalPrice, Villa.VillaName as ServiceTypeName
+from( Service
+ join Contract on Contract.ServiceId = Service.ServiceId )
+ join Villa on Villa.ServiceId =  Service.ServiceId
+ where Contract.ContractId not in (select Contract.ContractId from Service join Contract on Contract.ServiceId = Service.ServiceId where month(BeginDay) in (1,2,3) AND year(BeginDay) = '2019' )
+union all
+select Service.ServiceId, Service.ServiceName, Service.UseArea, Service.RentalPrice, Room.RoomName as ServiceTypeName
+from( Service
+ join Contract on Contract.ServiceId = Service.ServiceId )
+ join Room on Room.ServiceId =  Service.ServiceId
+ where Contract.ContractId not in (select Contract.ContractId from Service join Contract on Contract.ServiceId = Service.ServiceId where month(BeginDay) in (1,2,3) AND year(BeginDay) = '2019' )
+ union all
+ select Service.ServiceId, Service.ServiceName, Service.UseArea, Service.RentalPrice, House.HouseName as ServiceTypeName
+from( Service
+ join Contract on Contract.ServiceId = Service.ServiceId )
+ join House on House.ServiceId =  Service.ServiceId
+ where Contract.ContractId not in (select Contract.ContractId from Service join Contract on Contract.ServiceId = Service.ServiceId where month(BeginDay) in (1,2,3) AND year(BeginDay) = '2019' );
+ 
+ -- 7 Hiển thị thông tin IDDichVu, TenDichVu, DienTich, SoNguoiToiDa, ChiPhiThue, TenLoaiDichVu của tất cả các loại dịch vụ đã từng được Khách hàng đặt phòng trong năm 2018 nhưng chưa từng được Khách hàng đặt phòng  trong năm 2019.
+ 
+ select Service.ServiceId, Service.ServiceName, Service.UseArea, Service.MaxPeople, Service.RentalPrice, Villa.VillaName as ServiceTypeName
+from( Service
+ join Contract on Contract.ServiceId = Service.ServiceId )
+ join Villa on Villa.ServiceId =  Service.ServiceId
+ where Contract.ContractId in (select Contract.ContractId from Service join Contract on Contract.ServiceId = Service.ServiceId where year(BeginDay) = '2018' and not year(BeginDay) = '2019' )
+union all
+select Service.ServiceId, Service.ServiceName, Service.UseArea, Service.MaxPeople, Service.RentalPrice, Room.RoomName as ServiceTypeName
+from( Service
+ join Contract on Contract.ServiceId = Service.ServiceId )
+ join Room on Room.ServiceId =  Service.ServiceId
+ where Contract.ContractId in (select Contract.ContractId from Service join Contract on Contract.ServiceId = Service.ServiceId where year(BeginDay) = '2018' and not year(BeginDay) = '2019' )
+ union all
+ select Service.ServiceId, Service.ServiceName, Service.UseArea, Service.MaxPeople, Service.RentalPrice, House.HouseName as ServiceTypeName
+from( Service
+ join Contract on Contract.ServiceId = Service.ServiceId )
+ join House on House.ServiceId =  Service.ServiceId
+ where Contract.ContractId in (select Contract.ContractId from Service join Contract on Contract.ServiceId = Service.ServiceId where year(BeginDay) = '2018' and not year(BeginDay) = '2019' );
+ 
+ -- 8 Hiển thị thông tin HoTenKhachHang có trong hệ thống, với yêu cầu HoThenKhachHang không trùng nhau.
+ 
+select CustomerName from Customer
+group by CustomerName ;
+
+select distinct CustomerName from Customer;
+
+select CustomerName from Customer
+union
+select CustomerName from Customer;
+
+-- 9 Thực hiện thống kê doanh thu theo tháng, nghĩa là tương ứng với mỗi tháng trong năm 2019 thì sẽ có bao nhiêu khách hàng thực hiện đặt phòng.
+
+select month(BeginDay) as month, count(CustomerId) as Quantity, sum(TotalPayment) as Revenue from Contract
+where  year(BeginDay)='2019'
+group by month(BeginDay);
+
+-- 10 Hiển thị thông tin tương ứng với từng Hợp đồng thì đã sử dụng bao nhiêu Dịch vụ đi kèm. Kết quả hiển thị bao gồm IDHopDong, NgayLamHopDong, NgayKetthuc, TienDatCoc, SoLuongDichVuDiKem (được tính dựa trên việc count các IDHopDongChiTiet).
+
+select Contract.ContractId, Contract.BeginDay, Contract.FinishDay, Contract.Deposit, count(DetailContract.AccSerId) as Quantity 
+from Contract 
+inner join DetailContract on Contract.ContractId=DetailContract.ContractId
+group by DetailContract.AccSerId;
+
+-- 11 Hiển thị thông tin các Dịch vụ đi kèm đã được sử dụng bởi những Khách hàng có TenLoaiKhachHang là “Diamond” và có địa chỉ là “Vinh” hoặc “Quảng Ngãi”.
+
+select AccompaniedService.AccSerId, AccompaniedService.AccSerName, AccompaniedService.Unit, AccompaniedService.Price  
+from Customer 
+inner join Contract on Customer.CustomerId = Contract.CustomerId
+inner join DetailContract on Contract.ContractId = DetailContract.ContractId
+inner join AccompaniedService on DetailContract.AccSerId = AccompaniedService.AccSerId
+where Customer.Address like '%Vinh%' or Customer.Address like '%Quang Ngai%' and CustomerTypeId='Di';
+
+-- 12 Hiển thị thông tin IDHopDong, TenNhanVien, TenKhachHang, SoDienThoaiKhachHang, TenDichVu, SoLuongDichVuDikem (được tính dựa trên tổng Hợp đồng chi tiết), TienDatCoc của tất cả các dịch vụ đã từng được khách hàng đặt vào 3 tháng cuối năm 2019 nhưng chưa từng được khách hàng đặt vào 6 tháng đầu năm 2019
+
+select Contract.ContractId, Employee.EmployeeName, Customer.CustomerName, Customer.CusTelNum, Service.ServiceName, DetailContract.Quantity
+from Contract
+inner join Employee on Contract.EmployeeId = Employee.EmployeeId
+inner join Customer on Contract.CustomerId = Customer.CustomerId
+inner join Service on Contract.ServiceId = Service.ServiceId
+inner join DetailContract on Contract.ContractId = DetailContract.ContractId 
+where  (Contract.CustomerId  in (select CustomerId from Contract where
+ BeginDay between '2019-10-01' and '2019-12-31')) and (Contract.CustomerId not in (select CustomerId from Contract where
+ BeginDay between '2019-01-01' and '2019-06-30'));
+ 
+-- 13 Hiển thị thông tin các Dịch vụ đi kèm được sử dụng nhiều nhất bởi các Khách hàng đã đặt phòng.  
+select * , DetailContract.Quantity
+from AccompaniedService
+inner join DetailContract
+on AccompaniedService.AccSerId = DetailContract.AccSerId
+where DetailContract.Quantity= (select max(Quantity) from DetailContract);
+
+-- 14 Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. Thông tin hiển thị bao gồm IDHopDong, TenLoaiDichVu, TenDichVuDiKem, SoLanSuDung.
+
+select Contract.ContractId, Service.ServiceName, AccompaniedService.AccSerName, count(Contract.ContractId) from AccompaniedService
+inner join DetailContract on DetailContract.AccSerId = AccompaniedService.AccSerId
+inner join Contract on Contract.ContractId = DetailContract.ContractId
+inner join Service on Service.ServiceId = Contract.ServiceId
+group by AccompaniedService.AccSerId
+having count(Contract.ContractId) ='1';
+
+-- 15 Hiển thi thông tin của tất cả nhân viên bao gồm IDNhanVien, HoTen, TrinhDo, TenBoPhan, SoDienThoai, DiaChi mới chỉ lập được tối đa 3 hợp đồng từ năm 2018 đến 2019.
+
+select Employee.EmployeeId, Employee.EmployeeName, Diploma.DiplomaName, Department.DepartmentName, Employee.EmpTelNum, count(ContractId)
+from  Employee
+inner join Diploma on Diploma.DiplomaId = Employee.DiplomaId
+inner join Department on Department.DepartmentId = Employee.DepartmentId
+inner join Contract on Contract.EmployeeId = Employee.EmployeeId
+where year(BeginDay) in ('2018','2019')
+group by  Employee.EmployeeId
+having count(ContractId) <=3 ;
+
+-- 16 Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2017 đến năm 2019.
+
+-- 17 Cập nhật thông tin những khách hàng có TenLoaiKhachHang từ  Platinium lên Diamond, chỉ cập nhật những khách hàng đã từng đặt phòng với tổng Tiền thanh toán trong năm 2019 là lớn hơn 10.000.000 VNĐ.
+
+update Customer
+inner join Contract on Contract.CustomerId = Customer.CustomerId
+inner join CustomerType on CustomerType.CustomerTypeId = Customer.CustomerTypeId
+set Customer.CustomerTypeId = 'Di'
+where Customer.CustomerId in (select Contract.CustomerId from Contract
+ where Year(BeginDay) ='2019' and  Customer.CustomerTypeId = 'Pl'
+ group by  Customer.CustomerId
+having  sum(TotalPayment) > 10000000);
+
+-- 18 Xóa những khách hàng có hợp đồng trước năm 2016 (chú ý ràng buộc giữa các bảng).
+
+-- 19 Cập nhật giá cho các Dịch vụ đi kèm được sử dụng trên 10 lần trong năm 2019 lên gấp đôi.
+
+-- 20 Hiển thị thông tin của tất cả các Nhân viên và Khách hàng có trong hệ thống, thông tin hiển thị bao gồm ID (IDNhanVien, IDKhachHang), HoTen, Email, SoDienThoai, NgaySinh, DiaChi.
+
+select EmployeeId, EmployeeName, EmpEmail, EmpTelNum, EmployeeBir from Employee
+union all
+select CustomerId, CustomerName, CusEmail, CusTelNum, CustomerBir from Customer;
